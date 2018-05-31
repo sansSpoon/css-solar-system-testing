23860c766237ec0c58029a5ce875edafacd87725
sansSpoon <sans.spoon@gmail.com>
2018-05-31 16:27:25 +1000

    working through selects

--- public/js/test.js
+++ public/js/test.js
@@ -1,65 +1,60 @@
 const data = [
 	{
-		"name": "Solar",
+		"name": "foo",
 		"hierarchies": [
 			{
 				"name": "Alpha",
-				"planets": [
+				"children": [
 					{
-						"name": "Mercury",
-						"radiusKM": "2439.7",
-						"rotationVelocityKMH": "10.892",
-						"aphelionAU": "0.466697",
-						"perihelionAU": "0.307499",
-						"orbitVelocityKMS": "47.362"
+						"name": "A1",
+						"grandChildren": [
+							{
+								"name": "A1-a"
+							}
+						]
 					},
 					{
-						"name": "Venus",
-						"radiusKM": "6051.8",
-						"rotationVelocityKMH": "6.52",
-						"aphelionAU": "0.728213",
-						"perihelionAU": "0.718440",
-						"orbitVelocityKMS": "35.02"
-					},
+						"name": "A2"
+					}
 				],
-				"star": {
-					"name":"sol",
-					"radiusKM": "9000",
-					"rotationVelocityKMH": "700"
+				"property": {
+					"name":"Sub-Alpha"
+				}
+			},
+			{
+				"name": "Bravo",
+				"children": [
+					{
+						"name": "B1"
+					},
+					{
+						"name": "B2"
+					}
+				],
+				"property": {
+					"name":"Sub-Bravo"
 				}
 			}
 		]
 	}
 ]
 
-
-const system = d3.select('#galaxy').selectAll('div')
+const system = d3.select('body').selectAll('div')
 	.data(data)
 		.enter().append('div').classed("system", true)
 		
 const hierarchy = system.selectAll('.system')
-	.data(function(d) { console.log(d.hierarchies); return d.hierarchies })
+	.data(function(d, i) { console.log(d); return d.hierarchies })
 		.enter().append('div').classed("hierarchy", true)
-
+		.text(function(d, i) { return d.name })
 		
-const planets = hierarchy.selectAll('.hierarchy')
-	.data(function(d, i) { return d.planets })
-		.enter().append('div').classed("planet", true)
-		.style('width', function(d, i) { return `${d.radiusKM / 100}px` })
-		.style('height', function(d, i) { return `${d.radiusKM / 100}px` })
+const property = hierarchy.selectAll('.property')
+	.data(function(d) { return [d.property] })
+		.enter().append('div').classed("property", true)
+		.text(function(d) { return d.name })
+		
+const children = hierarchy.selectAll('.hierarchy')
+	.data(function(d, i) { return d.children })
+		.enter().append('div').classed("child", true)
 		.text(function(d, i) { return d.name })
 
-const sun = hierarchy.selectAll('.hierarchy')
-	.data(function(d, i) { console.log(d); return d })
-		.enter().append('div').classed("sun", true)
-
-
-
-
-
-//const sun = system.selectAll('.hierarchy')
-//	.data(function(d, i) { console.log(d.hierarchies); return d.hierarchies })
-//		.enter().append('div').classed("sun", true)
-//		.style('width', function(d, i) { return `${d.aphelionAU * 100}px` })
-//		.style('height', function(d, i) { return `${d.aphelionAU * 100}px` })
-//		.text(function(d, i) { return d.name })

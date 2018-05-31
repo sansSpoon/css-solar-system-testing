cc7166c984e8dfc7e2900133436a9a5fc2d8d167
sansSpoon <sans.spoon@gmail.com>
2018-05-31 16:47:47 +1000

    all data fully selected

--- public/js/test.js
+++ public/js/test.js
@@ -14,7 +14,8 @@ const data = [
 						]
 					},
 					{
-						"name": "A2"
+						"name": "A2",
+						"grandChildren": []
 					}
 				],
 				"property": {
@@ -25,10 +26,12 @@ const data = [
 				"name": "Bravo",
 				"children": [
 					{
-						"name": "B1"
+						"name": "B1",
+						"grandChildren": []
 					},
 					{
-						"name": "B2"
+						"name": "B2",
+						"grandChildren": []
 					}
 				],
 				"property": {
@@ -44,17 +47,48 @@ const system = d3.select('body').selectAll('div')
 		.enter().append('div').classed("system", true)
 		
 const hierarchy = system.selectAll('.system')
-	.data(function(d, i) { console.log(d); return d.hierarchies })
+	.data(function(d, i) { return d.hierarchies })
 		.enter().append('div').classed("hierarchy", true)
 		.text(function(d, i) { return d.name })
-		
+
+// Sun
 const property = hierarchy.selectAll('.property')
 	.data(function(d) { return [d.property] })
 		.enter().append('div').classed("property", true)
 		.text(function(d) { return d.name })
-		
+
+// Planets
 const children = hierarchy.selectAll('.hierarchy')
 	.data(function(d, i) { return d.children })
-		.enter().append('div').classed("child", true)
+		.enter().append('div').classed("child-container", true).append('div').classed("child-body", true)
 		.text(function(d, i) { return d.name })
+
+// moons
+const grandChildren = children.selectAll('.child-body')
+	.data(function(d, i) { console.log(d); return d.grandChildren })
+		.enter().append('div').classed("grand-child-container", true).append('div').classed("grand-child-body", true)
+		.text(function(d, i) { return d.name })
+
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
 

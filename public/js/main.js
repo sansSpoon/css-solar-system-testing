08e049995c0f790f74c818b499e900d34254e05c
sansSpoon <sans.spoon@gmail.com>
2018-06-01 17:13:21 +1000

    clean up controls and module config

--- public/js/main.js
+++ public/js/main.js
@@ -1,34 +1,13 @@
-const toggle2d = document.getElementById('toggle-2d');
-const toggle3d = document.getElementById('toggle-3d');
-const system = document.getElementById('system');
-
-
-toggle2d.onclick = () => {
-	system.classList.toggle('animate-2d');
-}
-toggle3d.onclick = () => {
-	system.classList.toggle('animate-3d');
-}
-
-
 const d3System = function(){
 
 	// Create the return module and its configuration
 	const module = {},
-		    config = {
-		css:{
-			classes:{
-				heading:'h1',
-				navbar:'nav-bar'
-			},
-			ids:{
-				widget:'widget',
-				block:'block'
-			} 
-		},
-		userName:'user',
-		token:'UUID'
-	};
+				config = {
+					ids:{
+						toggle2d: document.getElementById('toggle-2d'),
+						toggle3d: document.getElementById('toggle-3d'),
+					},
+				};
 		    data   = [{"hierarchies":[{"planets":[{"satellites":[],"_id":"5b0f8c4315315be81de5fcff","name":"Mercury","radiusKM":2439.7,"rotationVelocityKMH":10.892,"aphelionAU":0.466697,"perihelionAU":0.307499,"orbitVelocityKMS":47.362,"__v":0},{"satellites":[],"_id":"5b0f8c4315315be81de5fd00","name":"Venus","radiusKM":6051.8,"rotationVelocityKMH":6.52,"aphelionAU":0.728213,"perihelionAU":0.71844,"orbitVelocityKMS":35.02,"__v":0},{"satellites":[{"name":"Moon","radiusKM":1738.1,"rotationVelocityKMH":16.6572,"apoapsisAU":0.00270993162,"periapsisAU":0.00242383129,"orbitVelocityKMS":1.022,"_id":"5b0f8e02da4a86e83cbf6276"}],"_id":"5b0f8c4315315be81de5fd01","name":"Earth","radiusKM":6371,"rotationVelocityKMH":1674.4,"aphelionAU":1.017,"perihelionAU":0.98327,"orbitVelocityKMS":29.78,"__v":1},{"satellites":[],"_id":"5b0f8c4315315be81de5fd02","name":"Mars","radiusKM":3389.5,"rotationVelocityKMH":868.22,"aphelionAU":1.666,"perihelionAU":1.382,"orbitVelocityKMS":24.007,"__v":0},{"satellites":[],"_id":"5b0f8c4315315be81de5fd03","name":"Jupiter","radiusKM":69911,"rotationVelocityKMH":45000,"aphelionAU":5.4588,"perihelionAU":4.9501,"orbitVelocityKMS":13.07,"__v":0},{"satellites":[],"_id":"5b0f8c4315315be81de5fd04","name":"Saturn","radiusKM":58232,"rotationVelocityKMH":35500,"aphelionAU":10.1238,"perihelionAU":9.0412,"orbitVelocityKMS":9.68,"__v":0},{"satellites":[],"_id":"5b0f8c4315315be81de5fd05","name":"Uranus","radiusKM":25362,"rotationVelocityKMH":9320,"aphelionAU":20.11,"perihelionAU":18.33,"orbitVelocityKMS":6.8,"__v":0},{"satellites":[],"_id":"5b0f8c4315315be81de5fd06","name":"Neptune","radiusKM":24622,"rotationVelocityKMH":9650,"aphelionAU":30.33,"perihelionAU":29.81,"orbitVelocityKMS":5.43,"__v":0}],"name":"alpha","star":{"_id":"5b0f8c4315315be81de5fcfe","name":"Sol","radiusKM":695700,"rotationVelocityKMH":7189,"__v":0},"_id":"5b0f8ceeda4a86e83cbf6274"}],"_id":"5b0f8ceeda4a86e83cbf6275","name":"Solar System","__v":0}];
 
 	const system = d3.select('#galaxy').selectAll('div')
@@ -114,6 +93,15 @@ const d3System = function(){
 		
 		return mass;
 	}
+	
+	
+	// ! Private handlers
+	config.ids.toggle2d.onclick = () => {
+		system.classed('animate-2d', function() { return !d3.select(this).classed('animate-2d') });
+	}
+	config.ids.toggle3d.onclick = () => {
+		system.classed('animate-3d', function() { return !d3.select(this).classed('animate-3d') });
+	}
 
 	// ! Add functionality to the module's init()-ialising method
 	function init() {
@@ -127,6 +115,7 @@ const d3System = function(){
 	return module;
 
 }();
+
 
 // When the DOM is ready, run the module
 /*

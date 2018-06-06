458c88649788d37ea8cca4d0956b3de2b87db62d
sansSpoon <sans.spoon@gmail.com>
2018-06-07 09:57:58 +1000

    wrap in update method

--- public/js/main.js
+++ public/js/main.js
@@ -7,6 +7,7 @@ const d3System = function(){
 						universe: document.getElementById('universe'),
 						toggle2d: document.getElementById('toggle-2d'),
 						toggle3d: document.getElementById('toggle-3d'),
+						star: document.getElementById('starInput'),
 					},
 				},
 				data = [{"hierarchies":[{"planets":[{"satellites":[],"_id":"5b0f8c4315315be81de5fcff","name":"Mercury","radiusKM":2439.7,"rotationVelocityKMH":10.892,"aphelionAU":0.466697,"perihelionAU":0.307499,"orbitVelocityKMS":47.362,"__v":0},{"satellites":[],"_id":"5b0f8c4315315be81de5fd00","name":"Venus","radiusKM":6051.8,"rotationVelocityKMH":6.52,"aphelionAU":0.728213,"perihelionAU":0.71844,"orbitVelocityKMS":35.02,"__v":0},{"satellites":[{"name":"Moon","radiusKM":1738.1,"rotationVelocityKMH":16.6572,"apoapsisAU":0.00270993162,"periapsisAU":0.00242383129,"orbitVelocityKMS":1.022,"_id":"5b0f8e02da4a86e83cbf6276"}],"_id":"5b0f8c4315315be81de5fd01","name":"Earth","radiusKM":6371,"rotationVelocityKMH":1674.4,"aphelionAU":1.017,"perihelionAU":0.98327,"orbitVelocityKMS":29.78,"__v":1},{"satellites":[],"_id":"5b0f8c4315315be81de5fd02","name":"Mars","radiusKM":3389.5,"rotationVelocityKMH":868.22,"aphelionAU":1.666,"perihelionAU":1.382,"orbitVelocityKMS":24.007,"__v":0},{"satellites":[],"_id":"5b0f8c4315315be81de5fd03","name":"Jupiter","radiusKM":69911,"rotationVelocityKMH":45000,"aphelionAU":5.4588,"perihelionAU":4.9501,"orbitVelocityKMS":13.07,"__v":0},{"satellites":[],"_id":"5b0f8c4315315be81de5fd04","name":"Saturn","radiusKM":58232,"rotationVelocityKMH":35500,"aphelionAU":10.1238,"perihelionAU":9.0412,"orbitVelocityKMS":9.68,"__v":0},{"satellites":[],"_id":"5b0f8c4315315be81de5fd05","name":"Uranus","radiusKM":25362,"rotationVelocityKMH":9320,"aphelionAU":20.11,"perihelionAU":18.33,"orbitVelocityKMS":6.8,"__v":0},{"satellites":[],"_id":"5b0f8c4315315be81de5fd06","name":"Neptune","radiusKM":24622,"rotationVelocityKMH":9650,"aphelionAU":30.33,"perihelionAU":29.81,"orbitVelocityKMS":5.43,"__v":0}],"name":"alpha","star":{"_id":"5b0f8c4315315be81de5fcfe","name":"Sol","radiusKM":695700,"rotationVelocityKMH":7189,"__v":0},"_id":"5b0f8ceeda4a86e83cbf6274"}],"_id":"5b0f8ceeda4a86e83cbf6275","name":"Solar System","__v":0}];
@@ -17,7 +18,7 @@ const d3System = function(){
 	
 	console.log(orbits);
 	
-	const starfoo = data[0].hierarchies[0].star.radiusKM / 40000;
+	let starfoo = data[0].hierarchies[0].star.radiusKM / config.ids.star.value;
 	
 	console.log(`starfoo ${starfoo}`);
 
@@ -27,59 +28,72 @@ const d3System = function(){
 					
 	//console.log(orbitsScaled);
 				
-	const system = d3.select('#galaxy').selectAll('div')
-	.data(data).enter()
-		.append('div').attr('class', 'system')
-		.attr('id', function(d) { return d.name.replace(' ','-').toLowerCase(); })
-
-	// ! Hierarchy
-	const hierarchy = system.selectAll('.system')
-		.data(function(d) { return d.hierarchies }).enter()
-			.append('div').attr('class','hierarchy')
-
-	// ! Star
-	const star = hierarchy.selectAll('.star')
-		.data(function(d) { return [d.star] }).enter()
-			.append('div')
-				.attrs({
-					'class': 'star',
-					'id': function(d) { return d.name.replace(' ','-').toLowerCase(); },
-				})
-				.styles(function(d) { return _mass(d, type = 'star'); })
-
-	// ! Planets
-	const planets = hierarchy.selectAll('.hierarchy')
-		.data(function(d) { return d.planets }).enter()
-			.append('div')
-				.attrs({
-					'class': 'orbit',
-					'id': function(d) { return d.name.replace(' ','-').toLowerCase(); },
-				})
-				.styles(function(d) {
-					const parent = d3.select(this.parentNode).datum();
-					return _orbit(d, parent);
-				})
-			.append('div')
-				.attrs({'class': 'planet'})
-				.styles(function(d) { return _mass(d, type = 'planet'); })
-				//.text(function(d, i) { return d.name })
+	function _render(data) {
+		const system = d3.select('#galaxy').selectAll('div')
+		.data(data).enter()
+			.append('div').attr('class', 'system')
+			.attr('id', function(d) { return d.name.replace(' ','-').toLowerCase(); })
 	
-	// ! Satellites
-	const satellites = planets.selectAll('.planet')
-		.data(function(d) { return d.satellites }).enter()
-			.append('div')
-				.attrs({
-					'class': 'orbit',
-					'id': function(d) { return d.name.replace(' ','-').toLowerCase(); },
-				})
-				.styles(function(d) {
-					const parent = d3.select(this.parentNode).datum();
-					return _orbit(d, parent);
-				})
-			.append('div')
-				.attrs({'class': 'satellite'})
-				.styles(function(d) { return _mass(d, type = 'satellite'); })
-				//.text(function(d, i) { return d.name })
+	console.log('system done');
+	
+		// ! Hierarchy
+		const hierarchy = system.selectAll('.system')
+			.data(function(d) { return d.hierarchies }).enter()
+				.append('div').attr('class','hierarchy')
+	
+	console.log('hierarchy done');
+	
+		// ! Star
+		const star = hierarchy.selectAll('.star')
+			.data(function(d) { return [d.star] }).enter()
+				.append('div')
+					.attrs({
+						'class': 'star',
+						'id': function(d) { return d.name.replace(' ','-').toLowerCase(); },
+					})
+					.styles(function(d) { return _mass(d, type = 'star'); })
+	
+	console.log('star done');
+	
+		// ! Planets
+		const planets = hierarchy.selectAll('.hierarchy')
+			.data(function(d) { return d.planets }).enter()
+				.append('div')
+					.attrs({
+						'class': 'orbit',
+						'id': function(d) { return d.name.replace(' ','-').toLowerCase(); },
+					})
+					.styles(function(d) {
+						const parent = d3.select(this.parentNode).datum();
+						return _orbit(d, parent);
+					})
+				.append('div')
+					.attrs({'class': 'planet'})
+					.styles(function(d) { return _mass(d, type = 'planet'); })
+					//.text(function(d, i) { return d.name })
+		
+		console.log('planets done');
+		
+		// ! Satellites
+		const satellites = planets.selectAll('.planet')
+			.data(function(d) { return d.satellites }).enter()
+				.append('div')
+					.attrs({
+						'class': 'orbit',
+						'id': function(d) { return d.name.replace(' ','-').toLowerCase(); },
+					})
+					.styles(function(d) {
+						const parent = d3.select(this.parentNode).datum();
+						return _orbit(d, parent);
+					})
+				.append('div')
+					.attrs({'class': 'satellite'})
+					.styles(function(d) { return _mass(d, type = 'satellite'); })
+					//.text(function(d, i) { return d.name })
+					
+		console.log('satellites done');
+	
+	}
 
 
 
@@ -116,6 +130,9 @@ const d3System = function(){
 
 	
 	function _mass(d, type) {
+		
+		console.log(`starfoo in mass = ${starfoo}`)
+		
 		let unit = 'px';
 		let scale = type === 'star' ? 100000 : 1000;
 		let calc = Math.round((d.radiusKM) / scale);
@@ -156,11 +173,19 @@ const d3System = function(){
 	config.ids.toggle3d.onclick = () => {
 		system.classed('animate-3d', function() { return !d3.select(this).classed('animate-3d') });
 	}
+	
+	config.ids.star.onchange = () => {
+		console.log(config.ids.star.value);
+		starfoo = data[0].hierarchies[0].star.radiusKM / config.ids.star.value;
+		console.log(starfoo);
+		_render(data);
+	}
 
 	// ! Add functionality to the module's init()-ialising method
 	function init() {
 		//_moduleMethod();
-		_universeSize();
+		//_universeSize();
+		_render(data);
 	}
 
 	// ! Add init, and any other methods, to obj

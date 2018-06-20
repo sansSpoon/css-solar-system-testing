d088835481eef4e4850cba568d31081b4f725427
sansSpoon <sans.spoon@gmail.com>
2018-06-20 22:50:08 +1000

    fix merging, improve orbits, lerp working

--- public/js/main.js
+++ public/js/main.js
@@ -15,55 +15,59 @@ const d3System = function(){
 					},
 				},
 				data = [{"hierarchies":[{"planets":[{"satellites":[],"_id":"5b0f8c4315315be81de5fcff","name":"Mercury","radiusKM":2439.7,"rotationVelocityKMH":10.892,"aphelionAU":0.466697,"perihelionAU":0.307499,"orbitVelocityKMS":47.362,"__v":0},{"satellites":[],"_id":"5b0f8c4315315be81de5fd00","name":"Venus","radiusKM":6051.8,"rotationVelocityKMH":6.52,"aphelionAU":0.728213,"perihelionAU":0.71844,"orbitVelocityKMS":35.02,"__v":0},{"satellites":[{"name":"Moon","radiusKM":1738.1,"rotationVelocityKMH":16.6572,"apoapsisAU":0.00270993162,"periapsisAU":0.00242383129,"orbitVelocityKMS":1.022,"_id":"5b0f8e02da4a86e83cbf6276"}],"_id":"5b0f8c4315315be81de5fd01","name":"Earth","radiusKM":6371,"rotationVelocityKMH":1674.4,"aphelionAU":1.017,"perihelionAU":0.98327,"orbitVelocityKMS":29.78,"__v":1},{"satellites":[],"_id":"5b0f8c4315315be81de5fd02","name":"Mars","radiusKM":3389.5,"rotationVelocityKMH":868.22,"aphelionAU":1.666,"perihelionAU":1.382,"orbitVelocityKMS":24.007,"__v":0},{"satellites":[],"_id":"5b0f8c4315315be81de5fd03","name":"Jupiter","radiusKM":69911,"rotationVelocityKMH":45000,"aphelionAU":5.4588,"perihelionAU":4.9501,"orbitVelocityKMS":13.07,"__v":0},{"satellites":[],"_id":"5b0f8c4315315be81de5fd04","name":"Saturn","radiusKM":58232,"rotationVelocityKMH":35500,"aphelionAU":10.1238,"perihelionAU":9.0412,"orbitVelocityKMS":9.68,"__v":0},{"satellites":[],"_id":"5b0f8c4315315be81de5fd05","name":"Uranus","radiusKM":25362,"rotationVelocityKMH":9320,"aphelionAU":20.11,"perihelionAU":18.33,"orbitVelocityKMS":6.8,"__v":0},{"satellites":[],"_id":"5b0f8c4315315be81de5fd06","name":"Neptune","radiusKM":24622,"rotationVelocityKMH":9650,"aphelionAU":30.33,"perihelionAU":29.81,"orbitVelocityKMS":5.43,"__v":0}],"name":"alpha","star":{"_id":"5b0f8c4315315be81de5fcfe","name":"Sol","radiusKM":695700,"rotationVelocityKMH":7189,"__v":0},"_id":"5b0f8ceeda4a86e83cbf6274"}],"_id":"5b0f8ceeda4a86e83cbf6275","name":"Solar System","__v":0}];
-				
-	function lerp(n, x0, x1) {
-		// returns a position: x that is n percent between y0 and y1
-		// As orbits are x only, y values are fixed to 0(start) - 1(end)
-		const y0 = 1;
-		const y1 = 2;
-		const x = ((y1 - n)*x0 + (n - y0)*x1) / (y1 - y0);
-		
-		return x;
-	}
 
-	const orbits = data[0].hierarchies[0].planets.map((orbit) => {
-		return Math.round(((orbit.aphelionAU + orbit.perihelionAU) /2) * 10)
-	});
+	// Max planetary orbit
+	const planetMaxOrbit = Math.max(...data[0].hierarchies[0].planets.map(_apsisAvg));
+	console.log(planetMaxOrbit);
 	
-	console.log(orbits);
-	
-	const evenOrbits = orbits.map((value, index, arr) => {
-		return Math.round((Math.max(...arr)/arr.length) * (index + 1));
-	});
-	
-	console.log(evenOrbits);
-
-
-
+	// Count of planets
+	const planetCount = data[0].hierarchies[0].planets.length;
+	console.log(planetCount);
 
 	// THIS HAS TO CHANGE
 	let starfoo = data[0].hierarchies[0].star.radiusKM / config.ids.starScale.value;
 
 	// Scale
 	let orbitsScaled = d3.scaleLinear()
-		.domain([0, d3.max(orbits)])
+		.domain([0, planetMaxOrbit])
 		.range([starfoo, 90]);
 
 
-	// ! Private methods
-	function _orbit(d, i, nodes) {
-		console.log(i);
-		const unit = '%';
-		const a = d.hasOwnProperty('aphelionAU') ? d.aphelionAU : d.apoapsisAU;
-		const p = d.hasOwnProperty('perihelionAU') ? d.perihelionAU : d.periapsisAU;
+	// returns a position: x that is n percent between y0 and y1
+	// As orbits are x only, y values are fixed to 0(start) - 1(end)
+	function _lerp(n, x0, x1) {
+		const y0 = 1;
+		const y1 = 2;
+		const x = ((y1 - n)*x0 + (n - y0)*x1) / (y1 - y0);
 
-		const orbit = Math.round(((a + p) / 2) * 10); // *10 is just to have easier numbers to work with
+		return x;
+	}
+
+	// Average out a body's orbit and apply a scale
+	function _apsisAvg(body) {
+		const s = 10; // *10 is just to have easier numbers to work with
+		const a = body.hasOwnProperty('aphelionAU') ? body.aphelionAU : body.apoapsisAU;
+		const p = body.hasOwnProperty('perihelionAU') ? body.perihelionAU : body.periapsisAU;
+
+		return Math.round((a + p) / 2 * s);
+	}
+
+	// Apply lerp to orbits
+	function _orbit(d, i, nodes) {
+		console.log(d, i);
+		const unit = '%';
+		//const a = d.hasOwnProperty('aphelionAU') ? d.aphelionAU : d.apoapsisAU;
+		//const p = d.hasOwnProperty('perihelionAU') ? d.perihelionAU : d.periapsisAU;
+
+		//const orbit = Math.round(((a + p) / 2) * 10); // *10 is just to have easier numbers to work with
+		
+		const orbit = _apsisAvg(d);
 		console.log(orbit);
 		
-		const evenOrbit = Math.round((Math.max(...orbits)/orbits.length) * (i + 1));
+		const evenOrbit = Math.round((planetMaxOrbit/planetCount) * (i + 1));
 		console.log(evenOrbit);
 		
-		let scaledOrbit = Math.round(orbitsScaled(lerp(orbitScale.value, orbit, evenOrbit)));
+		let scaledOrbit = Math.round(orbitsScaled(_lerp(orbitScale.value, orbit, evenOrbit)));
 		console.log(scaledOrbit);
 
 		
@@ -128,9 +132,9 @@ const d3System = function(){
 		// add and update new systems
 		system = system.enter() // enter add div
 				.append('div')
-			.merge(system) // enter and update
 				.attr('class', 'system')
 				.attr('id', function(d) { return d.name.replace(' ','-').toLowerCase(); })
+			.merge(system) // enter and update
 
 
 		// ! Render Hierarchies
@@ -146,7 +150,8 @@ const d3System = function(){
 		// add and update new hierarchies
 		hierarchy = hierarchy.enter()
 				.append('div')
-				.attr('class','hierarchy');
+				.attr('class','hierarchy')
+			.merge(hierarchy);
 
 
 		// ! Render Stars
